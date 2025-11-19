ba5d8e212e6f02eca6e9fffa2b890182db19e902
wd2nf8gqct <committer@noreply.ethi.cz>
2025-11-18 22:26:09 -0500

    Config updates

--- zsh/.config/zsh/helpers.zsh
+++ zsh/.config/zsh/helpers.zsh
@@ -44,6 +44,18 @@ function fes() {
 }
 
 #***
+# yazi
+#***
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+
+#***
 # Create directory and enter it
 #***
 function mkcd() { mkdir -p "$@" && cd "$_" || exit; }
