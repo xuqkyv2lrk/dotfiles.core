833349dc705a7faa54fe5fade5569365bb2b518e
wd2nf8gqct <committer@noreply.ethi.cz>
2026-08-25 23:31:16 -0400

    feat(doom): hide markdown markup for cleaner display

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -61,6 +61,9 @@
 ;; Make frame a bit larger than default sat startup
 (setq initial-frame-alist '((top . 1) (left . 1) (width . 143) (height . 55)))
 
+(after! markdown-mode
+  (setq markdown-hide-markup t))
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
